6e57a2b891ff4d991ba5d3d5f29f91ab415f4744
Thommy C <tmc@thommysweb.com>
2019-09-24 17:03:36 +0200

    prepares download-function for csv file

--- whatsapp-numbers-csv.js
+++ whatsapp-numbers-csv.js
@@ -11,9 +11,19 @@ function getNumbers() {
     elements.forEach(element => {
         VALIDPREFIXES.forEach(prefix => {
             if (element.title.startsWith(prefix)) {
-                numbers = numbers + element.title + "\n";
+                numbers = numbers + element.title + '\n';
             }
         });
     });
     return numbers;
+}
+
+function downloadCSV(content) {
+    var csv = new Blob([content], { type: 'text/csv' });
+    var link = document.createElement('a');
+    link.style.display = 'none';
+    link.download = FILENAME;
+    link.href = window.URL.createObjectURL(csv);
+    document.body.appendChild(link);
+    link.click();
 }
