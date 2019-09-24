313c433cbcdb6d9bef84d6e49c9d3d10a18bf24d
Thommy C <tmc@thommysweb.com>
2019-09-24 16:49:24 +0200

    retrieves all numbers from span elements with a valid prefix and writes them to a string, seperated by newline

--- whatsapp-numbers-csv.js
+++ whatsapp-numbers-csv.js
@@ -4,3 +4,16 @@ const VALIDPREFIXES = [
     '+43'
 ];
 const FILENAME = 'whatsappNumbers_' + new Date(Date.now()).toISOString().replace('T', '_').substring(0, 16) + '.csv'
+
+function getNumbers() {
+    numbers = "";
+    elements = document.querySelectorAll('span[title]');
+    elements.forEach(element => {
+        VALIDPREFIXES.forEach(prefix => {
+            if (element.title.startsWith(prefix)) {
+                numbers = numbers + element.title + "\n";
+            }
+        });
+    });
+    return numbers;
+}
