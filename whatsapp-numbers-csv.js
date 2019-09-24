2aa2846821355787987f8c5bd55bf62e5686d453
Thommy C <tmc@thommysweb.com>
2019-09-24 16:41:43 +0200

    sets the filename & valid prefixes for numbers

---
+++ whatsapp-numbers-csv.js
@@ -0,0 +1,6 @@
+const VALIDPREFIXES = [
+    '+49', 
+    '+41', 
+    '+43'
+];
+const FILENAME = 'whatsappNumbers_' + new Date(Date.now()).toISOString().replace('T', '_').substring(0, 16) + '.csv'
